e178454d1f880d6ccb50e88ec60047b7250e96d7
wenjialu <50645596+wenjialu@users.noreply.github.com>
2021-04-15 15:19:01 +0800

    Delete account directory

--- account/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from blocks.models import Block, Post, Comment
-from account.models import Profile
-
-# Register your models here.
-admin.site.register(Block)
-admin.site.register(Post)
-admin.site.register(Comment)
-admin.site.register(Profile)
